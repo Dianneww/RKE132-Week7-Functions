d1a2de4c0d4edf9515c4229510ea17f573cb08f0
Dianneww <diana.lagoida@tktk.ee>
2023-02-19 16:56:19 +0200

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+//PrintHello();
+//PrintGoodbye();
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
